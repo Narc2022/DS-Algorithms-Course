4aa65e59693eb7e66aaa3eba9d30da0040ca02cd
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-08-17 08:50:42 +0530

    Update 3Anagram.js

--- 3Anagram.js
+++ 3Anagram.js
@@ -5,6 +5,7 @@
 //lenght check (for both Strings)
 //String hello
 //{h:0,e:0,l:0,e:0}
+//complexity : linear (On)
 
 function isAnagram(str1, str2) {
   if (str1.length != str2.length) {
@@ -14,8 +15,15 @@ function isAnagram(str1, str2) {
   for (let letter of str1) {
     count[letter] = (count[letter] || 0) + 1;
   }
-  console.log("count", count);
+
+  for (let item of str2) {
+    if (!count[item]) {
+      return false;
+    }
+    count[item] -= 1;
+  }
+  return true;
 }
 
-const result = isAnagram("hello", "llheo");
+const result = isAnagram("hello", "lldeo");
 console.log("result", result);
